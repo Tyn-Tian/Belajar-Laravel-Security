1544231885f7daef313cf1395aa8f4240eaf5040
Christian <kristian2015ok@gmail.com>
2024-06-03 00:08:02 +0700

    Add: Blade Template

--- tests/Feature/TodoControllerTest.php
+++ tests/Feature/TodoControllerTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\Todo;
 use App\Models\User;
 use Database\Seeders\TodoSeeder;
 use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class TodoControllerTest extends TestCase
@@ -22,5 +24,32 @@ class TodoControllerTest extends TestCase
         $this->actingAs($user)
             ->post('/api/todo')
             ->assertStatus(200);
+    }
+
+    public function testView()
+    {
+        $this->seed([UserSeeder::class, TodoSeeder::class]);
+        $user = User::where("email", "tian@localhost")->firstOrFail();
+        Auth::login($user);
+
+        $todos = Todo::query()->get();
+
+        $this->view('todos', [
+            "todos" => $todos
+        ])->assertSeeText("Edit")
+            ->assertSeeText("Delete")
+            ->assertDontSeeText("No Edit")
+            ->assertDontSeeText("No Delete");
+    }
+
+    public function testViewGuest()
+    {
+        $this->seed([UserSeeder::class, TodoSeeder::class]);
+        $todos = Todo::query()->get();
+
+        $this->view('todos', [
+            "todos" => $todos
+        ])->assertSeeText("No Edit")
+            ->assertSeeText("No Delete");
     }
 }
